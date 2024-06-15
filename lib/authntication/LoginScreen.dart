32898ad64aa99016ed1d128eeb7a7f9b15c948fa
Girish Jagadish Sonune <116879201+GirishSonune@users.noreply.github.com>
2024-06-15 08:55:32 +0530

    Update LoginScreen.dart
    Change in UI.

--- lib/authntication/LoginScreen.dart
+++ lib/authntication/LoginScreen.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:attendance_management_system_ams/Dashboard/DashBoard.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -54,168 +55,229 @@ class LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery
+        .of(context)
+        .size;
     return Scaffold(
       body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
+        margin: EdgeInsets.only(top: 40),
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [
-              hexStringToColor("CB2B93"),
-              hexStringToColor("9546C4"),
-              hexStringToColor("5E61F4"),
-            ],
             begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xffF4EEF2),
+              Color(0xffF4EEF2),
+              Color(0xffE3EDF5),
+            ],
           ),
         ),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(
-                15, MediaQuery.of(context).size.height * 0.1, 20, 0),
-            child: Column(
-              children: <Widget>[
-                imageWidget("assets/applogo/amslogo.png"),
-                const SizedBox(height: 30),
+        child: SafeArea(
+            child: ListView(
+              children: [
+                SizedBox(height: size.height * 0.03),
+                const Text(
+                  "Hello User!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 37,
+                    color: Color(0xff353047),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  "Wellcome to Zidio Development \n Attendance App",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 27, color: Color(0xff6F6B7A), height: 1.2),
+                ),
+                SizedBox(height: size.height * 0.04),
+                // for username and password
                 reusableTextField("Enter Email", Icons.email_outlined, false,
                     emailTextController),
-                const SizedBox(height: 20),
                 reusableTextField("Enter Password", Icons.lock_outline, true,
                     passwordTextController),
-                const SizedBox(height: 20),
-                signInSignUpButton(
-                  context: context,
-                  text: 'LOG IN',
-                  onTap: _login,
+                SizedBox(height: size.height * 0.04),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Column(
+                    children: [
+                      // for sign in button
+                      InkWell(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 20),
+                          margin: EdgeInsets.only(top: 40),
+                          decoration: BoxDecoration(
+                            color: const Color(0xffFD6B68),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 22,
+                              ),
+                            ),
+                          ),
+                        ),
+                        onTap: _login,
+                      ),
+                      SizedBox(height: size.height * 0.07),
+                      InkWell(
+                        child: const Text.rich(
+                          TextSpan(
+                              text: "New User? ",
+                              style: TextStyle(
+                                color: Color(0xff6F6B7A),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                              children: [ TextSpan(
+                                text: "Register now",
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              ]
+                          ),
+                        ),
+                        onTap: (){
+                          Navigator.push(
+                              context, MaterialPageRoute(builder: (context) => const SignupScreen()));
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-                signUpOption(context),
-                adminLoginOption(context),
               ],
+            )),
+      ),
+    );
+  }
+
+
+// Function to convert hex color string to Color
+  Color hexStringToColor(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF$hexColor";
+    }
+    if (hexColor.length == 8) {
+      return Color(int.parse("0x$hexColor"));
+    }
+    return Colors.black; // Default to black if there's an issue
+  }
+
+  Padding reusableTextField(String text, IconData icon, bool isPasswordType,
+      TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 25,
+        vertical: 10,
+      ),
+      child: TextField(
+        controller: controller,
+        obscureText: isPasswordType,
+        enableSuggestions: !isPasswordType,
+        autocorrect: !isPasswordType,
+        cursorColor: Colors.black26,
+        style: TextStyle(color: Colors.black26.withOpacity(0.9)),
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            icon,
+            color: Colors.black26,
+          ),
+          labelText: text,
+          labelStyle: TextStyle(color: Colors.black26.withOpacity(0.9)),
+          filled: true,
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          fillColor: Colors.white.withOpacity(0.3),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30.0),
+            borderSide: const BorderSide(width: 0, style: BorderStyle.none),
+          ),
+        ),
+        keyboardType: isPasswordType
+            ? TextInputType.visiblePassword
+            : TextInputType.emailAddress,
+      ),
+    );
+  }
+
+// SignInSignUpButton widget as defined previously
+  Container signInSignUpButton({
+    required BuildContext context,
+    required String text,
+    required VoidCallback onTap,
+  }) {
+    return Container(
+      width: MediaQuery
+          .of(context)
+          .size
+          .width,
+      height: 50,
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(90),
+      ),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.resolveWith((states) {
+            if (states.contains(WidgetState.pressed)) {
+              return Colors.black26;
+            }
+            return Colors.white;
+          }),
+          shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
             ),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.black87,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
           ),
         ),
       ),
     );
   }
-}
 
-// Function to convert hex color string to Color
-Color hexStringToColor(String hexColor) {
-  hexColor = hexColor.toUpperCase().replaceAll("#", "");
-  if (hexColor.length == 6) {
-    hexColor = "FF$hexColor";
+  Row signUpOption(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      const Text(
+          "Don't have an account?", style: TextStyle(color: Colors.white)),
+      GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SignupScreen()));
+        },
+        child: const Text(" Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      )
+    ]);
   }
-  if (hexColor.length == 8) {
-    return Color(int.parse("0x$hexColor"));
+
+  Row adminLoginOption(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      const Text("Admin?", style: TextStyle(color: Colors.white)),
+      GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(
+              builder: (context) => const AdminLoginScreen()));
+        },
+        child: const Text(" Login as Admin",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      )
+    ]);
   }
-  return Colors.black; // Default to black if there's an issue
-}
-
-// Reusable Widgets (imageWidget and reusableTextField)
-Widget imageWidget(String imageName) {
-  return Image.asset(
-    imageName,
-    fit: BoxFit.fitWidth,
-    width: 240,
-    height: 240,
-    color: Colors.white,
-  );
-}
-
-TextField reusableTextField(String text, IconData icon, bool isPasswordType,
-    TextEditingController controller) {
-  return TextField(
-    controller: controller,
-    obscureText: isPasswordType,
-    enableSuggestions: !isPasswordType,
-    autocorrect: !isPasswordType,
-    cursorColor: Colors.white,
-    style: TextStyle(color: Colors.white.withOpacity(0.9)),
-    decoration: InputDecoration(
-      prefixIcon: Icon(
-        icon,
-        color: Colors.white70,
-      ),
-      labelText: text,
-      labelStyle: TextStyle(color: Colors.white.withOpacity(0.9)),
-      filled: true,
-      floatingLabelBehavior: FloatingLabelBehavior.never,
-      fillColor: Colors.white.withOpacity(0.3),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30.0),
-        borderSide: const BorderSide(width: 0, style: BorderStyle.none),
-      ),
-    ),
-    keyboardType: isPasswordType
-        ? TextInputType.visiblePassword
-        : TextInputType.emailAddress,
-  );
-}
-
-// SignInSignUpButton widget as defined previously
-Container signInSignUpButton({
-  required BuildContext context,
-  required String text,
-  required VoidCallback onTap,
-}) {
-  return Container(
-    width: MediaQuery.of(context).size.width,
-    height: 50,
-    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(90),
-    ),
-    child: ElevatedButton(
-      onPressed: onTap,
-      style: ButtonStyle(
-        backgroundColor: WidgetStateProperty.resolveWith((states) {
-          if (states.contains(WidgetState.pressed)) {
-            return Colors.black26;
-          }
-          return Colors.white;
-        }),
-        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
-          ),
-        ),
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(
-          color: Colors.black87,
-          fontWeight: FontWeight.bold,
-          fontSize: 16,
-        ),
-      ),
-    ),
-  );
-}
-
-Row signUpOption(BuildContext context) {
-  return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-    const Text("Don't have an account?", style: TextStyle(color: Colors.white)),
-    GestureDetector(
-      onTap: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => const SignupScreen()));
-      },
-      child: const Text(" Sign Up",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-    )
-  ]);
-}
-
-Row adminLoginOption(BuildContext context) {
-  return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-    const Text("Admin?", style: TextStyle(color: Colors.white)),
-    GestureDetector(
-      onTap: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => const AdminLoginScreen()));
-      },
-      child: const Text(" Login as Admin",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-    )
-  ]);
 }
